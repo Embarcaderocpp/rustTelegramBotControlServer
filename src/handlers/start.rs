cdfd89d8a8a9017e8980de016f5cf82e240b40f4
sha-va <embarcaderocpp@gmail.com>
2026-04-03 20:03:57 +0300

    edit start

--- src/handlers/start.rs
+++ src/handlers/start.rs
@@ -1,6 +1,8 @@
 use teloxide::{Bot, prelude::*, RequestError};
+use teloxide::utils::command::BotCommands;
+use crate::handlers::commands::Command;
 
 pub async fn start(bot: Bot, msg: Message) -> Result<(), RequestError> {
-    bot.send_message(msg.chat.id, "You press Start!").await?;
+    bot.send_message(msg.chat.id, Command::descriptions().to_string()).await?;
     Ok(())
 }
